67b38fa2190d7c028677639872a43a7f82c5f69a
Joshua Morony <joshua.morony@gmail.com>
2024-08-29 11:24:50 +0930

    feat(agx-remark-rehype): add test case placeholders

--- packages/agx-remark-rehype/src/lib/agx-remark-rehype.spec.ts
+++ packages/agx-remark-rehype/src/lib/agx-remark-rehype.spec.ts
@@ -1,7 +1,15 @@
 import { agxRemarkRehype } from './agx-remark-rehype';
 
 describe('agxRemarkRehype', () => {
-  it('should work', () => {
-    expect(agxRemarkRehype()).toEqual('agx-remark-rehype');
-  });
+  it('should return standard markdown unmodified', () => {});
+
+  it('should escape characters that break Angular templates', () => {});
+
+  it('should allow embedding components without escaping', () => {});
+
+  it('should allow Angular control flow syntax without escaping', () => {});
+
+  it('should allow supplying remark plugins', () => {});
+
+  it('should allow supplying rehype plugins', () => {});
 });
