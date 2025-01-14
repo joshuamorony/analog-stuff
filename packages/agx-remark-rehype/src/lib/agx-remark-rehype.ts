5b888670674e6e8ba26c15f1507ddc69688a355c
Joshua Morony <joshua.morony@gmail.com>
2025-01-14 20:18:27 +1030

    Revert "fix(agx-remark-rehype): escape parentheses in templates"
    This reverts commit bfd9c687a7e15e599ecc8e25b812bff97b7d5860.

--- packages/agx-remark-rehype/src/lib/agx-remark-rehype.ts
+++ packages/agx-remark-rehype/src/lib/agx-remark-rehype.ts
@@ -42,8 +42,6 @@ const fixDoubleEscape = (content: string) => {
     .replace(/&#x26;#64;/g, '&#64;')
     .replace(/&#x26;#x2774;/g, '&lbrace;')
     .replace(/&#x26;#96;/g, '&#96;')
-    .replace(/&#x26;lpar;/g, '&lpar;')
-    .replace(/&#x26;rpar;/g, '&rpar;')
     .replace(/&#x26;#x2775;/g, '&rbrace;');
 };
 
@@ -80,7 +78,6 @@ const escapeBreakingCharacters = (code: string) => {
   // such as @, {, and ,}.
   code = code.replace(/@/g, '&#64;');
   code = code.replace(/{/g, '&#x2774;').replace(/}/g, '&#x2775;');
-  code = code.replace(/\(/g, '&lpar;').replace(/\)/g, '&rpar;');
   code = code.replace(/`/g, '&#96;');
 
   return code;
